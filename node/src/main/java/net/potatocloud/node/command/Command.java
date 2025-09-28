777068aacd8a4036082908d9caea117ca86feffb
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-09-28 13:54:13 +0200

    Added SubCommand system

--- node/src/main/java/net/potatocloud/node/command/Command.java
+++ node/src/main/java/net/potatocloud/node/command/Command.java
@@ -1,7 +1,9 @@
 package net.potatocloud.node.command;
 
 import lombok.Getter;
+import net.potatocloud.node.Node;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,6 +14,8 @@ public abstract class Command {
     private final String description;
     private final List<String> aliases;
 
+    private final List<SubCommand> subCommands;
+
     protected Command() {
         final CommandInfo info = this.getClass().getAnnotation(CommandInfo.class);
         if (info == null) {
@@ -20,8 +24,22 @@ public abstract class Command {
         name = info.name();
         description = info.description();
         aliases = Arrays.asList(info.aliases());
+        subCommands = new ArrayList<>();
     }
 
     public abstract void execute(String[] args);
 
+    public SubCommand getSubCommand(String name) {
+        return subCommands.stream().filter(subCommand -> subCommand.getName().equals(name)).findFirst().orElse(null);
+    }
+
+    public void addSubCommand(SubCommand subCommand) {
+        subCommands.add(subCommand);
+    }
+
+    protected void sendHelp() {
+        for (SubCommand subCommand : subCommands) {
+            Node.getInstance().getLogger().info(subCommand.getUsage() + " &8- &7" + subCommand.getDescription());
+        }
+    }
 }

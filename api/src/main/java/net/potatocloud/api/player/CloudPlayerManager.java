57b9e229e05d2f5b820c8870c26dcab9c204a341
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-10-23 13:55:24 +0200

    Add JavaDoc for cloud player classes

--- api/src/main/java/net/potatocloud/api/player/CloudPlayerManager.java
+++ api/src/main/java/net/potatocloud/api/player/CloudPlayerManager.java
@@ -9,32 +9,84 @@ import java.util.stream.Collectors;
 
 public interface CloudPlayerManager {
 
+    /**
+     * Gets a player by its username.
+     *
+     * @param username the name of the player
+     * @return the player
+     */
     CloudPlayer getCloudPlayer(String username);
 
+    /**
+     * Gets a player by its unique id.
+     *
+     * @param uniqueId the unique id of the player
+     * @return the player
+     */
     CloudPlayer getCloudPlayer(UUID uniqueId);
 
+    /**
+     * Gets the set of all online players.
+     *
+     * @return the set of all online players
+     */
     Set<CloudPlayer> getOnlinePlayers();
 
+    /**
+     * Gets the set of all online players that are connected to the given service group.
+     *
+     * @param group the service group
+     * @return the set of all online players that are connected to the given service group
+     */
     default Set<CloudPlayer> getOnlinePlayersByGroup(ServiceGroup group) {
         return getOnlinePlayers().stream()
                 .filter(player -> player.getConnectedService() != null && player.getConnectedService().getServiceGroup().getName().equals(group.getName()))
                 .collect(Collectors.toSet());
     }
 
+    /**
+     * Connects the player to the given service.
+     *
+     * @param playerName the name of the player to connect
+     * @param serviceName the name of the service
+     */
     void connectPlayerWithService(String playerName, String serviceName);
 
+    /**
+     * Connects the player to the given service.
+     *
+     * @param player the player to connect
+     * @param serviceName the name of the service
+     */
     default void connectPlayerWithService(CloudPlayer player, String serviceName) {
         connectPlayerWithService(player.getUsername(), serviceName);
     }
 
+    /**
+     * Connects the player to the given service.
+     *
+     * @param playerName the name of the player to connect
+     * @param service the service to connect with
+     */
     default void connectPlayerWithService(String playerName, Service service) {
         connectPlayerWithService(playerName, service.getName());
     }
 
+    /**
+     * Connects the player to the given service.
+     *
+     * @param player the player to connect
+     * @param service the service to connect with
+     */
     default void connectPlayerWithService(CloudPlayer player, Service service) {
         connectPlayerWithService(player, service.getName());
     }
 
+    /**
+     * Updates an existing player.
+     *
+     * @param player the player to update
+     */
     void updatePlayer(CloudPlayer player);
 
 }

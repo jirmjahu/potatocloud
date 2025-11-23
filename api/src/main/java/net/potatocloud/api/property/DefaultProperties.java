19413a1b3b19d4b85a57ceec55b09b8d3e000abc
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-11-23 15:59:05 +0100

    Changed default value for Game State property

--- api/src/main/java/net/potatocloud/api/property/DefaultProperties.java
+++ api/src/main/java/net/potatocloud/api/property/DefaultProperties.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class DefaultProperties {
 
-    public static final Property<String> GAME_STATE = Property.ofString("gameState", "INGAME");
+    public static final Property<String> GAME_STATE = Property.ofString("gameState", "LOBBY");
     public static final Property<Boolean> VELOCITY_MODERN_FORWARDING = Property.ofBoolean("velocityModernForwarding", false);
     public static final Property<Boolean> ALWAYS_OVERRIDE_FORWARDING_SECRET = Property.ofBoolean("alwaysOverrideForwardingSecret", true);
 

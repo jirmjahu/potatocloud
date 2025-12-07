a64ed5b9132a6e3e57d34cc7eb0a61c232d71133
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-12-07 18:48:29 +0100

    Update version to 1.4.0

--- api/src/main/java/net/potatocloud/api/CloudAPI.java
+++ api/src/main/java/net/potatocloud/api/CloudAPI.java
@@ -21,7 +21,7 @@ public abstract class CloudAPI {
     /**
      * The current version.
      */
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
 
     public CloudAPI() {
         instance = this;

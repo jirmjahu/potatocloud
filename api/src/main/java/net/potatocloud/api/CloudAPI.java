dbf91bd0a8f7ec38cb4acbad013bcafe95a16cf1
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-12-21 16:40:21 +0100

    Updated version to: 1.4.1

--- api/src/main/java/net/potatocloud/api/CloudAPI.java
+++ api/src/main/java/net/potatocloud/api/CloudAPI.java
@@ -21,7 +21,7 @@ public abstract class CloudAPI {
     /**
      * The current version.
      */
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
 
     public CloudAPI() {
         instance = this;
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "net.potatocloud"
-version = "1.4.0"
+version = "1.4.1"
 
 allprojects {
     apply(plugin = "java")
